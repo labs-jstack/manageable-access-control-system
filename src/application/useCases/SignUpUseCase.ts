00ebab2d0a543d4e11ecbff17aa808b8a163e862
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-02-21 17:18:27 -0300

    feat: create signup use case

---
+++ src/application/useCases/SignUpUseCase.ts
@@ -0,0 +1,33 @@
+import { hash } from 'bcryptjs';
+import { AccountAlreadyExists } from '../errors/AccountAlreadyExists';
+import { prismaClient } from '../libs/prismaClient';
+
+interface IInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type IOutput = void
+
+export class SignUpUseCase {
+  async execute({ email, name, password }: IInput): Promise<IOutput> {
+    const accountAlreadyExists = await prismaClient.account.findUnique({
+      where: {
+        email
+      }
+    });
+    if (accountAlreadyExists) {
+      throw new AccountAlreadyExists();
+    }
+    const hashedPassword = await hash(password, 10);
+    await prismaClient.account.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword
+      }
+    });
+    return;
+  }
+}
