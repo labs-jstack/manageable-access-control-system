00e18a33676fdf01eaf20fcce94aac9c49cbb669
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-02-24 17:00:56 -0300

    feat: create route adapter

---
+++ src/server/adapters/routeAdapter.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { IController } from '../../application/interfaces/IController';
+
+export function routeAdapter(controller: IController) {
+  return async (request: Request, response: Response) => {
+    const { body, statusCode } = await controller.handle({
+      body: request.body,
+      params: request.params
+    });
+    response.status(statusCode).json(body);
+  };
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,14 @@
 import Express from 'express';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { routeAdapter } from './adapters/routeAdapter';
 
 const express = Express();
 
 express.use(Express.json());
 
-express.post('/sign-up', async (request, response) => {
-  const sigUpController = makeSignUpController();
-  const { body, statusCode } = await sigUpController.handle({ body: request.body });
-  response.status(statusCode).json(body);
-});
-
-express.post('/sign-in', async (request, response) => {
-  const signInController = makeSignInController();
-  const { body, statusCode } = await signInController.handle({body: request.body});
-  response.status(statusCode).json(body);
-});
+express.post('/sign-up', routeAdapter(makeSignUpController()));
+express.post('/sign-in', routeAdapter(makeSignInController()));
 
 express.listen(3001, () => {
   console.log('> Server started at http://localhost:3001');
